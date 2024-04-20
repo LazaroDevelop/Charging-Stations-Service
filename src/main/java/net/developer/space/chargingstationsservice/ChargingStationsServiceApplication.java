eec6328aeba80516add931384c34b218ed669375
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-04-20 18:02:29 -0400

    initial commit

---
+++ src/main/java/net/developer/space/chargingstationsservice/ChargingStationsServiceApplication.java
@@ -0,0 +1,13 @@
+package net.developer.space.chargingstationsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChargingStationsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChargingStationsServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/developer/space/chargingstationsservice/ChargingStationsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.developer.space.chargingstationsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChargingStationsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
