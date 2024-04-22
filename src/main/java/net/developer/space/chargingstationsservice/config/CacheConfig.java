5e6f2246b7b3e5e5bf5016aed02f4e851896e855
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-04-21 21:49:55 -0400

    add and enable cache config

---
+++ src/main/java/net/developer/space/chargingstationsservice/config/CacheConfig.java
@@ -0,0 +1,8 @@
+package net.developer.space.chargingstationsservice.config;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfig {}
