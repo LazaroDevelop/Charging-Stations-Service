8cdd0d7fc2a15c1d22950645aaad3ce54d7f7a00
LazaroDevelop <146339590+LazaroDevelop@users.noreply.github.com>
2024-04-23 15:49:40 -0400

    refactoring charging station controller and unitary tests

--- src/main/java/net/developer/space/chargingstationsservice/controller/ChargingStationController.java
+++ src/main/java/net/developer/space/chargingstationsservice/controller/ChargingStationController.java
@@ -1,4 +1,4 @@
-package net.developer.space.chargingstationsservice;
+package net.developer.space.chargingstationsservice.controller;
 
 import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
